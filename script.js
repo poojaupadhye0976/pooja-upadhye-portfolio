6bb0a2552f1d105ec919ca556563c214c5a8f8eb
poojaupadhye0976 <pooja.upadhye@pinnacle.in>
2024-12-28 13:48:17 +0530

    added collapsible functionality in script.js #8

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.collapsible').forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active');
+        const content = button.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+});
